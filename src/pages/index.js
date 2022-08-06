fc972bf93dc4e4a72a7c61a1cbf1e255d61f734c
Serik Ayazbyaev <jetheez@yandex.ru>
2022-08-06 20:43:33 +0600

    feat:  добавить форму

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 import './index.scss';
 import '../scripts/accardionCreate';
+import '../scripts/form';
---
+++ src/scripts/form.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+(function (d, w, c) {
+    (w[c] = w[c] || []).push({formId:186501,host:"formdesigner.ru",formHeight:100, el: "form_186501_1",center: 1,scroll: 0});
+    var s = d.createElement("script"), g = "getElementsByTagName";
+    s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//formdesigner.ru/js/iform.js?v=0.0.3";
+    var h=d[g]("head")[0] || d[g]("body")[0];
+    h.appendChild(s);
+})(document, window, "fdforms");
